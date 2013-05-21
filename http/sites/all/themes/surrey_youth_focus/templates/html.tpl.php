a70cb677eaf7604ce7dd3f15a5c77efc88302b52
Rik Barwick <rik@creativitea.co.uk>
2013-05-21 12:14:00 +0100

    full download of live server

---
+++ http/sites/all/themes/surrey_youth_focus/templates/html.tpl.php
@@ -0,0 +1,21 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+ 
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+  <script type="text/javascript" src="http://twitter.com/javascripts/blogger.js"></script>
+  <script type="text/javascript" src="http://api.twitter.com/1/statuses/user_timeline/SurreyYF.json?callback=twitterCallback2&count=1"></script>
+</body>
+</html>
