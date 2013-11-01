1bda5b68cc5df72bae5d68122f4aff08866dd938
Rik Barwick <rik@creativitea.co.uk>
2013-11-01 18:26:58 +0000

    added login text

--- public/sites/all/themes/surrey_youth_focus/template.php
+++ public/sites/all/themes/surrey_youth_focus/template.php
@@ -10,3 +10,17 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+ <?php
+function mytheme_theme(&$existing, $type, $theme, $path) {
+  $hooks['user_login'] = array(
+    'template' => 'templates/user_login',
+    'render element' => 'form',
+    // other theme registration code...
+  );
+  return $hooks;
+}
+function mytheme_preprocess_user_login(&$variables) {
+  $variables['intro_text'] = t('This is my awesome login form');
+  $variables['rendered'] = drupal_render_children($variables['form']);
+}
+?>
